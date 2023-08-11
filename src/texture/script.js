a3738ce211caf414d13d707edb6df83a6093525c
Nick_전세호 <shjeon@maxst.com>
2023-08-11 19:32:16 +0900

    텍스쳐 입히기와 조작들

---
+++ src/texture/script.js
@@ -0,0 +1,154 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+
+THREE.ColorManagement.enabled = false;
+
+// const image = new Image();
+// const texture = new THREE.Texture(image);
+
+// image.onload = () => {
+//   texture.needsUpdate = true;
+// };
+
+// image.src = "/textures/door/color.jpg";
+
+const loadingManager = new THREE.LoadingManager();
+
+loadingManager.onStart = () => {
+  console.log("start");
+};
+loadingManager.onLoad = () => {
+  console.log("loading");
+};
+loadingManager.onProgress = () => {
+  console.log("progress");
+};
+loadingManager.onError = () => {
+  console.log("error");
+};
+
+const textureLoader = new THREE.TextureLoader(loadingManager);
+const colorTexture = textureLoader.load(
+  "/textures/door/color.jpg"
+  //   () => {
+  //     console.log("load");
+  //   },
+  //   () => {
+  //     console.log("progress");
+  //   },
+  //   () => {
+  //     console.log("error");
+  //   }
+);
+const alphaTexture = textureLoader.load("/textures/door/alpha.jpg");
+const heightTexture = textureLoader.load("/textures/door/height.jpg");
+const normalTexture = textureLoader.load("/textures/door/normal.jpg");
+const ambientOcclusionTexture = textureLoader.load(
+  "/textures/door/ambientOcclusion.jpg"
+);
+const metalnessTexture = textureLoader.load("/textures/door/metalness.jpg");
+const roughnessTexture = textureLoader.load("/textures/door/roughness.jpg");
+const mineTexture = textureLoader.load("/textures/minecraft.png");
+
+ambientOcclusionTexture.repeat.x = 2;
+ambientOcclusionTexture.repeat.y = 3;
+ambientOcclusionTexture.wrapS = THREE.RepeatWrapping;
+ambientOcclusionTexture.wrapT = THREE.RepeatWrapping;
+
+ambientOcclusionTexture.offset.x = 0.5;
+ambientOcclusionTexture.offset.y = 0.5;
+
+ambientOcclusionTexture.rotation = Math.PI / 4;
+ambientOcclusionTexture.center.x = 0.5;
+ambientOcclusionTexture.center.y = 0.5;
+
+mineTexture.minFilter = THREE.NearestFilter;
+mineTexture.magFilter = THREE.NearestFilter;
+
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// Scene
+const scene = new THREE.Scene();
+
+/**
+ * Object
+ */
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ map: mineTexture });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.x = 1;
+camera.position.y = 1;
+camera.position.z = 1;
+scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.outputColorSpace = THREE.LinearSRGBColorSpace;
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock();
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+
+  // Update controls
+  controls.update();
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
