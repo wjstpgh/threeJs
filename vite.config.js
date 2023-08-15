ef938fd732aeaa80aab462e727156cd74714eefa
wjstpgh <sehoJ3756@gmail.com>
2023-08-15 22:12:09 +0900

    live server deploy를 위한 vercel setting과 multipage deploy를 위한 vite설정 추가 및 push시 자동 빌드, 배포를 위한 husky세팅 추가

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,33 @@
-const isCodeSandbox = 'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env
+import { resolve } from "path";
+
+const isCodeSandbox =
+  "SANDBOX_URL" in process.env || "CODESANDBOX_HOST" in process.env;
 
 export default {
-    root: 'src/',
-    publicDir: '../static/',
-    base: './',
-    server:
-    {
-        host: true,
-        open: !isCodeSandbox // Open if it's not a CodeSandbox
+  root: "src/",
+  publicDir: "../static/",
+  base: "./",
+  server: {
+    host: true,
+    open: !isCodeSandbox, // Open if it's not a CodeSandbox
+  },
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+    sourcemap: true,
+    rollupOptions: {
+      input: {
+        home: resolve(__dirname, "src/index.html"),
+        "3dtext": resolve(__dirname, "src/3dtext/index.html"),
+        animation: resolve(__dirname, "src/animation/index.html"),
+        camera: resolve(__dirname, "src/camera/index.html"),
+        debug: resolve(__dirname, "src/debug/index.html"),
+        geometries: resolve(__dirname, "src/geometries/index.html"),
+        material: resolve(__dirname, "src/material/index.html"),
+        screenresize: resolve(__dirname, "src/screenresize/index.html"),
+        texture: resolve(__dirname, "src/texture/index.html"),
+        transform: resolve(__dirname, "src/transform/index.html"),
+      },
     },
-    build:
-    {
-        outDir: '../dist',
-        emptyOutDir: true,
-        sourcemap: true
-    }
-}
+  },
+};
