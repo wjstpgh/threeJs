059fcdb89d93b21876249d394065fa3451ff523f
wjstpgh <sehoJ3756@gmail.com>
2023-10-08 19:36:09 +0900

    클래스 싱글톤 구조를 사용한 코드 구조화

---
+++ src/codeStructing/experience/sources.js
@@ -0,0 +1,29 @@
+export default [
+  {
+    name: "environmentMapTexture",
+    type: "cubeTexture",
+    path: [
+      "/textures/environmentMaps/3/px.jpg",
+      "/textures/environmentMaps/3/nx.jpg",
+      "/textures/environmentMaps/3/py.jpg",
+      "/textures/environmentMaps/3/ny.jpg",
+      "/textures/environmentMaps/3/pz.jpg",
+      "/textures/environmentMaps/3/nz.jpg",
+    ],
+  },
+  {
+    name: "grassColorTexture",
+    type: "texture",
+    path: "/textures/dirt/color.jpg",
+  },
+  {
+    name: "grassNormalTexture",
+    type: "texture",
+    path: "/textures/dirt/normal.jpg",
+  },
+  {
+    name: "foxModel",
+    type: "gltfModel",
+    path: "/models/Fox/glTF/Fox.gltf",
+  },
+];
