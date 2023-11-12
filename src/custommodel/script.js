d9fc52ea9147ebc62123e223ffb1fcb670ebd395
wjstpgh <sehoJ3756@gmail.com>
2023-11-12 16:03:06 +0900

    블렌더로 제작한 모델 로드

--- src/custommodel/script.js
+++ src/custommodel/script.js
@@ -51,7 +51,7 @@ scene.add(floor);
 /**
  * Lights
  */
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.8);
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.4);
 scene.add(ambientLight);
 
 const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6);
